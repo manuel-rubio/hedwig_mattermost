bfd9c9e4b5d5934fc5ee9de6c5eb2b07554dde08
Troels Brødsgaard <troels@uptime.dk>
2017-11-17 21:54:44 +0100

    Add Hex package metadata

--- mix.exs
+++ mix.exs
@@ -2,13 +2,17 @@ defmodule HedwigMattermost.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :hedwig_mattermost,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :hedwig_mattermost,
+      version: "0.0.1",
+      elixir: "~> 1.3",
+      elixirc_paths: elixirc_paths(Mix.env),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: "Mattermost adapter for the Hedwig bot framework.",
+      package: package(),
+    ]
   end
 
   # Configuration for the OTP application
@@ -22,7 +26,7 @@ defmodule HedwigMattermost.Mixfile do
         :websocket_client,
         :httpoison,
       ],
-      mod: {HedwigMattermost, []}
+      mod: {HedwigMattermost, []},
     ]
   end
 
@@ -51,4 +55,15 @@ defmodule HedwigMattermost.Mixfile do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Troels Brødsgaard"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/trarbr/hedwig_mattermost"
+      },
+    ]
+  end
 end
