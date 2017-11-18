6e5e5e5243a7c34b40fbf075be2de7aee36a9784
Troels Brødsgaard <troels@uptime.dk>
2017-11-18 07:39:22 +0100

    Update version to 0.1 since Hex package works :cake:

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HedwigMattermost.Mixfile do
   def project do
     [
       app: :hedwig_mattermost,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
